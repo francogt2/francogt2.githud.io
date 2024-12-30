ce27f5508dbbf9613c6f7b59581a27d0398c3e71
francogt2 <francogruppi1@gmail.com>
2024-12-29 23:21:31 -0300

    Update productsList.js

--- js/productsList.js
+++ js/productsList.js
@@ -4,7 +4,7 @@ const products = [
     name: "Celular Samsung Galaxy A05",
     description: "128GB, 4GM RAM, 5000mAh, 50MP",
     price: 1990,
-    img: '../img/1.jpeg'
+    img: 'img/1.jpeg'
   },
   {
     id: 2,
@@ -18,27 +18,27 @@ const products = [
     name: "Notebook Asus Vivobook",
     description: "16GB, 512GB SSD, AMD Ryzen 7",
     price: 12999,
-    img: '../img/3.jpeg'
+    img: 'img/3.jpeg'
   },
   {
     id: 4,
     name: "Notebook HP Victus",
     description: "16GB, 1TB SDD, NVIDIA RTX 4060",
     price: 15999,
-    img: '../img/4.jpeg'
+    img: 'img/4.jpeg'
   },
   {
     id: 5,
     name: "Tablet Samsung Galaxy Tab A9",
     description: "64GB, 4GB, 8,7",
     price: 2299,
-    img: '../img/5.jpeg'
+    img: 'img/5.jpeg'
   },
   {
     id: 6,
     name: "Tablet Lenovo M11",
     description: "8BG, 8GB RAM, 11",
     price: 3199,
-    img: '../img/6.jpeg'
+    img: 'img/6.jpeg'
   }
 ]
